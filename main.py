e994751e94b2968ed710016eb70c668f46b0ea6e
Wolfiii <54332169+DiniskaWolfiii@users.noreply.github.com>
2024-05-27 21:07:31 +0200

    inital commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, send_from_directory
+import os
+
+app = Flask(__name__, static_folder='client/build')
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else: return send_from_directory(app.static_folder, 'index.html')
+
+if __name__ == '__main__':
+    app.run()
