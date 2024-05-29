238dda8238104edf41ed6b5e4d83b8500c90eb1a
Wolfiii <54332169+DiniskaWolfiii@users.noreply.github.com>
2024-05-29 23:46:06 +0200

    tet

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="first-header">Welcome to my Homepage!</h1>
+        <h1 className="first-header">Welcome to my Homepage! :)</h1>
         <h2 className="second-header">This website is currently under construction...</h2>
         <h3 className="third-header">In the meantime, check out this <a href="https://www.youtube.com/watch?v=coaN2VBNgYA">spinning chip!</a></h3>
       </header>
